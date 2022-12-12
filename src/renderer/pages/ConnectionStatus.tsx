dc5ea827285bcec0b7b8d0cf74bf93eddf2a1524
하지훈 <jnh03336@gmail.com>
2022-12-12 09:50:45 +0900

    [FE] feat: DB 연결 화면

--- src/renderer/pages/ConnectionStatus.tsx
+++ src/renderer/pages/ConnectionStatus.tsx
@@ -1,5 +1,93 @@
+import { useState } from 'react';
+import {
+  List,
+  ListItem,
+  Column,
+  ErrorMessage,
+  Input,
+  Button,
+} from 'renderer/CSScontainers';
+
+interface DBstatus {
+  user: string;
+  host: string;
+  database: string;
+  password: string;
+  port: string;
+}
+
+const defaultDBinfo =
+  process.env.NODE_ENV === 'development'
+    ? {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: '123',
+        port: '5432',
+      }
+    : { user: '', host: '', database: '', password: '', port: '' };
+
 const ConnectionStatus = () => {
-  return <div>connection status</div>;
+  const [connected, setConnected] = useState(false);
+  const [dbInfo, setInfo] = useState<DBstatus>(defaultDBinfo);
+  const [error, setError] = useState(false);
+  const getInput = (e: any) => {
+    setInfo({ ...dbInfo, [e.target.name]: e.target.value });
+  };
+
+  const connectionSuccess = () => {
+    setConnected(true);
+  };
+  const connectionFail = () => {
+    setError(true);
+    setTimeout(() => {
+      setError(false);
+    }, 3000);
+  };
+  const onClickConnection = async () => {
+    const res = await db.command.request('makeConnection', [dbInfo]);
+    if (res === false) connectionFail();
+    if (res === true) connectionSuccess();
+  };
+  const fields: ['user', 'host', 'database', 'password', 'port'] = [
+    'user',
+    'host',
+    'database',
+    'password',
+    'port',
+  ];
+  if (!connected) {
+    return (
+      <Column>
+        {fields.map((field) => (
+          <Input
+            value={dbInfo[field]}
+            key={field}
+            name={field}
+            placeholder={field}
+            onChange={getInput}
+          />
+        ))}
+        <Button type="button" onClick={onClickConnection}>
+          접속하기
+        </Button>
+        <ErrorMessage style={{ opacity: error ? 1 : 0 }}>
+          정확하지 않은 접속정보입니다.
+        </ErrorMessage>
+      </Column>
+    );
+  }
+  return (
+    <Column>
+      <List>
+        {fields.map((field) => (
+          <ListItem key={field} style={{ padding: 0 }}>
+            {field} : {dbInfo[field]}
+          </ListItem>
+        ))}
+      </List>
+    </Column>
+  );
 };
 
 export default ConnectionStatus;
