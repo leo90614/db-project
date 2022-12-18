ebbd896c92dd0b4816c9468fdc8087de4ba0b153
sturdyChair <95079186+sturdyChair@users.noreply.github.com>
2022-12-18 16:03:37 +0900

    수정할 테이블을 arg[0], 속성을 arg[1], 타입을 arg[2]로

--- src/main/features/a/a3/modifyFeatureDataType.ts
+++ src/main/features/a/a3/modifyFeatureDataType.ts
@@ -5,6 +5,12 @@ export default function (ipcMain: Electron.IpcMain): void {
   const channelName = 'modifyFeatureDataType';
   ipcMain.handle(channelName, async (event, arg) => {
     try {
+      const modTable = arg[0];
+      const colName = arg[1];
+      const colType = arg[2];
+      const sqlStr = 'ALTER TABLE ';
+      const modSQL = sqlStr.concat(modTable, ' MODIFY ',colName, ' ', colType, ';');
+      const res = await dbClient.sql(modSQL);
       return success();
     } catch {
       return error('');
