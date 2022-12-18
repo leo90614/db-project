ef15dce221f86964d3abf5cab56dca7cfac86483
sturdyChair <95079186+sturdyChair@users.noreply.github.com>
2022-12-18 15:56:50 +0900

    Update deleteFeature.ts
    arg[0]을 삭제할 테이블 이름, arg[1]을 속성 이름으로 받게 만들었습니다

--- src/main/features/a/a3/deleteFeature.ts
+++ src/main/features/a/a3/deleteFeature.ts
@@ -5,9 +5,14 @@ export default function (ipcMain: Electron.IpcMain): void {
   const channelName = 'deleteFeature';
   ipcMain.handle(channelName, async (event, arg) => {
     try {
-      return success();
+      const deleteTable = arg[0];
+      const col = arg[1];
+      const sqlStr = 'ALTER TABLE ';
+      const deleteSQL = sqlStr.concat(deleteTable,' DROP COLUMN ', col, ';');
+      const res = await dbClient.sql(deleteSQL);
+      return success(0, 'delete succeed');
     } catch {
-      return error('');
+      return error('delete fail');
     }
   });
 }
