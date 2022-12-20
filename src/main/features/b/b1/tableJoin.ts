a28a107d409e4b5bb16529e899107a11c40eb0c5
leo90614 <103626058+leo90614@users.noreply.github.com>
2022-12-20 20:05:21 +0900

    Update tableJoin.ts

--- src/main/features/b/b1/tableJoin.ts
+++ src/main/features/b/b1/tableJoin.ts
@@ -1,6 +1,22 @@
 import dbClient from '../../../instance/dbClient';
 import { error, success } from '../../../lib/status';
 
+SELECT *
+ FROM tableA
+          INNER JOIN tableB
+          ON tableA.ID = tableB.key_ID
+
+
+CREATE VIEW VIEW_NAME AS
+SELECT 테이블명, 레코드 수, 결합키 속 성명, 대표 결합키
+FROM table_name
+WHERE condition;
+
+SELECT * FROM class.select_test
+INTO OUTFILE '/home/stricky/select_csv/select_test.csv'
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
+
 export default function (ipcMain: Electron.IpcMain): void {
   const channelName = 'tableJoin';
   ipcMain.handle(channelName, async (event, arg) => {
